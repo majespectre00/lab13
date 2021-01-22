316ca8adbc6244bf858eb4b4efc0e0efe80a49df
majespectre00 <oat601_12@hotmail.com>
2021-01-22 17:28:38 +0700

    lab13_1 completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+	for(int i = 1;i < N;i++){
+		T temp = d[i];
+		for(int j = i;j>=0;j--){
+			if(j == 0) {d[j] = temp; break;}
+			if(temp>d[j-1]) d[j]=d[j-1];
+			else {
+				d[j] = temp;
+				break;}
+		}
+		cout<<"Pass "<<i<<":";
+		for(int i = 0;i<N;i++){
+			cout<<d[i]<<" ";}
+		cout <<endl;
+	}
 }
 
 int main(){
